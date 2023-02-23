9dc50078ae249ba77a8f510a031eea16ab8e255d
KyuSang Cho <ksang0324@gmail.com>
2023-02-23 16:30:27 +0900

    20230223

---
+++ src/Main13.java
@@ -0,0 +1,12 @@
+public class Main13 {
+
+    public static void main(String[] args) {
+
+        Solution13 solution13 = new Solution13();
+
+        int[] ns = {20, 100};
+        for (int n : ns) System.out.println(solution13.solution(n));
+
+    }
+
+}
---
+++ src/Solution13.java
@@ -0,0 +1,10 @@
+class Solution13 {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i=1; i<=Math.sqrt(n); i++) {
+            if (n/i == Math.sqrt(n)) answer++;
+            else if (n%i==0) answer+=2;
+        }
+        return answer;
+    }
+}
