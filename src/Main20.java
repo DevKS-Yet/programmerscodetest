3d6dda9d673be599b330178ba5289131af847196
KyuSang Cho <ksang0324@gmail.com>
2023-03-07 15:36:43 +0900

    20230307

---
+++ src/Main20.java
@@ -0,0 +1,13 @@
+public class Main20 {
+
+    public static void main(String[] args) {
+
+        Solution20 solution20 = new Solution20();
+
+        int[] ns = {7, 1, 15};
+
+        for (int n : ns) System.out.println(solution20.solution(n));
+
+    }
+
+}
