2fc20b6da38ba8765d5e4a2c9c83e823be8a8f4f
KyuSang Cho <ksang0324@gmail.com>
2023-03-08 13:38:21 +0900

    20230308

---
+++ src/Main21.java
@@ -0,0 +1,13 @@
+public class Main21 {
+
+    public static void main(String[] args) {
+
+        Solution21 solution21 = new Solution21();
+
+        String[] my_strings = {"jaron", "bread"};
+
+        for (String my_string : my_strings) System.out.println(solution21.solution(my_string));
+
+    }
+
+}
