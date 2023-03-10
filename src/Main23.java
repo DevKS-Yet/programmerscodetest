d3bb51a15ca55017a5687cff9e32284bd742d9a8
KyuSang Cho <ksang0324@gmail.com>
2023-03-10 14:32:27 +0900

    20230310

---
+++ src/Main23.java
@@ -0,0 +1,13 @@
+public class Main23 {
+
+    public static void main(String[] args) {
+
+        Solution23 solution23 = new Solution23();
+
+        int[][] numberz = { {1, 2, 3, 4, 5}, {0, 31, 24, 10, 1, 9} };
+
+        for (int[] numbers : numberz) System.out.println(solution23.solution(numbers));
+
+    }
+
+}
