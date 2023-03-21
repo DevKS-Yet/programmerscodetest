e55a874ab87f34d3c3588af8fad9077e5888c500
DevKS-Yet <ksang_v@naver.com>
2023-03-21 23:19:43 +0900

    20230321

---
+++ src/Main29.java
@@ -0,0 +1,13 @@
+public class Main29 {
+
+    public static void main(String[] args) {
+
+        Solution29 solution29 = new Solution29();
+
+        String[] messages = { "happy birthday!", "I love you~"};
+
+        for (String message : messages) System.out.println(solution29.solution(message));
+
+    }
+
+}
