f88e21a3fb5975d5a32c02374692e97b595438bc
KyuSang Cho <ksang0324@gmail.com>
2023-03-22 15:00:26 +0900

    20230322

---
+++ src/Main30.java
@@ -0,0 +1,13 @@
+public class Main30 {
+
+    public static void main(String[] args) {
+
+        Solution30 solution30 = new Solution30();
+
+        int[] ns = {1234, 930211};
+
+        for (int n : ns) System.out.println(solution30.solution(n));
+
+    }
+
+}
